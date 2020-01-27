9c301a5e181ce1d4b655a526114793b87db12259
Rashmi Gupta <rashmi224141@gmail.com>
2020-01-27 14:36:07 +0530

    Implemented post request
    added date formatting mechanism
    added content and name formatting mechanism

--- templates/template.js
+++ templates/template.js
@@ -2,7 +2,7 @@ const commentDetail = `
 <div class="commentDetail">
 <div class="subscriber"><img src="img/icon.png" height="25" width="25" class="icon"/>
 <p class="name">__name__</p></div>
-<p class="commentDate">Date: __date__</p>
+<p class="commentDate">__date__</p>
   <p class="comment">__comment__</p>
   </div>
 `;
@@ -17,7 +17,7 @@ const guestBook = `<!DOCTYPE html>
     <h1 class="heading"><a href="./index.html">&#60;&#60;</a> Guest Book</h1>
     <hr />
     <h1>Leave a comment</h1>
-    <form method="GET">
+    <form method="POST">
       <label> Name: </label>
       <input type="text" name="name" required/> <br /><br />
       <label> Comment:</label>
