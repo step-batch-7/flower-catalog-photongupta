7d954023212f643ecb19b474b6c59c6d346fca76
Rashmi Gupta <rashmi224141@gmail.com>
2020-01-29 19:30:12 +0530

    Added app class

--- server.js
+++ server.js
@@ -1,18 +1,9 @@
 const http = require('http');
-const {findHandler} = require('./lib/app');
-
-const handleRequest = function(req, res) {
-  const remote = `req from port ${req.socket.remotePort}`;
-  req.setEncoding('utf8');
-  req.on('close', () => console.warn(`${remote}:closed `));
-  req.on('error', err => console.error('Error', err));
-  const handler = findHandler(req);
-  handler(req, res);
-};
+const {app} = require('./lib/handler');
 
 const main = function(port = 8000) {
   const server = http.createServer();
-  server.on('request', handleRequest);
+  server.on('request', app.serve.bind(app));
   server.listen(port, () => console.log('server is listing', server.address()));
 };
 
