77dd66287173957d93740950c707cc09bea7af3f
Rashmi Gupta <rashmi224141@gmail.com>
2020-01-29 16:14:52 +0530

    used http

--- server.js
+++ server.js
@@ -1,22 +1,18 @@
-const {Server} = require('net');
+const http = require('http');
 const {handleData} = require('./lib/app');
 
-const handleConnection = function(socket) {
-  const remote = `${socket.remoteAddress}is listening on port ${socket.remotePort}`;
-  socket.setEncoding('utf8');
-  socket.on('close', () => console.warn(`${remote} closed `));
-  socket.on('end', () => console.warn(`${remote} ended`));
-  socket.on('error', err => console.error('Error', err));
-  socket.on('data', text => handleData(text, socket));
+const handleRequest = function(req, res) {
+  const remote = `req from port ${req.socket.remotePort}`;
+  req.setEncoding('utf8');
+  req.on('close', () => console.warn(`${remote}:closed `));
+  req.on('error', err => console.error('Error', err));
+  handleData(req, res);
 };
 
 const main = function(port = 8000) {
-  const server = new Server();
-  server.on('connection', handleConnection);
-  server.on('listening', () =>
-    console.warn('server is listing', server.address())
-  );
-  server.listen(port);
+  const server = http.createServer();
+  server.on('request', handleRequest);
+  server.listen(port, () => console.log('server is listing', server.address()));
 };
 
 main(9000);
