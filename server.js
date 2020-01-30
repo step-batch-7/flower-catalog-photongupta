3089bda307103ec3898b8d6317cd58185daa7b55
Rashmi Gupta <rashmi224141@gmail.com>
2020-01-30 10:34:11 +0530

    added eslint and refactored the code

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
+'use strict';
+
 const http = require('http');
 const {app} = require('./lib/handler');
+const defaultPort = 5000;
 
-const main = function(port = 8000) {
+const main = function(port = defaultPort) {
   const server = http.createServer();
   server.on('request', app.serve.bind(app));
-  server.listen(port, () => console.log('server is listing', server.address()));
+  server.listen(port, () =>
+    process.stdout.write('server is listing', server.address())
+  );
 };
 
-main(9000);
+main();
